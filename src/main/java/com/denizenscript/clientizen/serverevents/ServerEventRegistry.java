1c3ad80d3d2a0213d2edbba51e89ef8070b6ba69
Aya <31237389+tal5@users.noreply.github.com>
2022-09-20 16:12:50 +0300

    Remove `PlayerPressesKey` server event

--- src/main/java/com/denizenscript/clientizen/serverevents/ServerEventRegistry.java
+++ src/main/java/com/denizenscript/clientizen/serverevents/ServerEventRegistry.java
@@ -3,6 +3,5 @@ package com.denizenscript.clientizen.serverevents;
 public class ServerEventRegistry {
 
 	public static void registerEvents() {
-		ServerEventManager.registerEvent(PlayerPressesKey.class);
 	}
 }
