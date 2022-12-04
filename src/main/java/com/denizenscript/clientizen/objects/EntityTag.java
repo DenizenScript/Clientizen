047a355ed20fe1fb83380ebaaa89a25fbf581fce
Aya <31237389+tal5@users.noreply.github.com>
2022-12-04 20:27:35 +0000

    `EntityTag` cleanup and fixes

--- src/main/java/com/denizenscript/clientizen/objects/EntityTag.java
+++ src/main/java/com/denizenscript/clientizen/objects/EntityTag.java
@@ -3,51 +3,93 @@ package com.denizenscript.clientizen.objects;
 import com.denizenscript.denizencore.objects.Fetchable;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.tags.Attribute;
 import com.denizenscript.denizencore.tags.ObjectTagProcessor;
 import com.denizenscript.denizencore.tags.TagContext;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
 import java.util.UUID;
 
 public class EntityTag implements ObjectTag {
 
 	public UUID uuid;
-
-	public EntityTag(UUID uuid) {
-		this.uuid = uuid;
-	}
+	public Entity entity;
 
 	public EntityTag(Entity entity) {
-		this(entity.getUuid());
+		this.entity = entity;
+		this.uuid = entity.getUuid();
 	}
 
 	@Fetchable("e")
-	public static EntityTag valueOf(String string, TagContext tagContext) {
+	public static EntityTag valueOf(String string, TagContext context) {
 		if (string == null) {
 			return null;
 		}
 		if (string.startsWith("e@")) {
-			return new EntityTag(UUID.fromString(string.substring(2)));
+			string = string.substring("e@".length());
 		}
-		if (tagContext == null || tagContext.showErrors()) {
+		try {
+			Entity found = MinecraftClient.getInstance().world.getEntityLookup().get(UUID.fromString(string));
+			if (found != null) {
+				return new EntityTag(found);
+			}
+		}
+		catch (Exception ignored) {}
+		if (context == null || context.showErrors()) {
 			Debug.log("valueOf EntityTag returning null: " + string);
 		}
 		return null;
 	}
 
+	public static boolean matches(String string) {
+		return string.startsWith("e@");
+	}
+
 	public Entity getEntity() {
-		return MinecraftClient.getInstance().world.getEntityLookup().get(uuid);
+		if (entity == null || entity.isRemoved()) {
+			Entity found = MinecraftClient.getInstance().world.getEntityLookup().get(uuid);
+			if (found != null) {
+				entity = found;
+			}
+		}
+		return entity;
+	}
+
+	public static void register() {
+		tagProcessor.registerTag(ElementTag.class, "entity_type", (attribute, object) -> {
+			return new ElementTag(object.getEntity().getType().getUntranslatedName(), true);
+		});
+		tagProcessor.registerTag(ElementTag.class, "health", (attribute, object) -> {
+			if (object.getEntity() instanceof LivingEntity livingEntity) {
+				return new ElementTag(livingEntity.getHealth());
+			}
+			return null;
+		});
 	}
 
 	public static ObjectTagProcessor<EntityTag> tagProcessor = new ObjectTagProcessor<>();
 
-	public static void registerTags() {
+	@Override
+	public ObjectTag getObjectAttribute(Attribute attribute) {
+		return tagProcessor.getObjectAttribute(this, attribute);
+	}
 
-		tagProcessor.registerTag(ElementTag.class, "entity_type", (attribute, object) -> {
-			return new ElementTag(object.getEntity().getType().getUntranslatedName(), true);
-		});
+	@Override
+	public String identify() {
+		return "e@" + uuid;
+	}
+
+	@Override
+	public String identifySimple() {
+		return identify();
+	}
+
+	@Override
+	public String toString() {
+		return identify();
 	}
 
 	private String prefix = "Entity";
@@ -60,16 +102,6 @@ public class EntityTag implements ObjectTag {
 	@Override
 	public boolean isUnique() {
 		return true;
-	}
-
-	@Override
-	public String identify() {
-		return "e@" + uuid;
-	}
-
-	@Override
-	public String identifySimple() {
-		return identify();
 	}
 
 	@Override
