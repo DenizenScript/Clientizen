ceee24571fad5ccb35201d9e9a5597a9d942b73b
Aya <31237389+tal5@users.noreply.github.com>
2022-12-05 10:36:22 +0000

    Cleanup imports

--- src/main/java/com/denizenscript/clientizen/tags/ClientizenTagRegistry.java
+++ src/main/java/com/denizenscript/clientizen/tags/ClientizenTagRegistry.java
@@ -1,9 +1,5 @@
 package com.denizenscript.clientizen.tags;
 
-import com.denizenscript.clientizen.objects.EntityTag;
-import com.denizenscript.denizencore.objects.ObjectFetcher;
-import com.denizenscript.denizencore.objects.ObjectType;
-
 public class ClientizenTagRegistry {
 
 	public static void registerTagHandlers() {
