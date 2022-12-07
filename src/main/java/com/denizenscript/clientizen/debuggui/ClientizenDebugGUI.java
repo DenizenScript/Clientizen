eb19bf05fee24dec829e64fffe818dfdedc795de
Aya <31237389+tal5@users.noreply.github.com>
2022-12-07 20:05:39 +0000

    Add some comments

--- src/main/java/com/denizenscript/clientizen/debuggui/ClientizenDebugGUI.java
+++ src/main/java/com/denizenscript/clientizen/debuggui/ClientizenDebugGUI.java
@@ -13,19 +13,25 @@ import net.minecraft.text.Text;
 public class ClientizenDebugGUI extends LightweightGuiDescription {
 
 	public ClientizenDebugGUI() {
+		// current size of the screen
 		int width = MinecraftClient.getInstance().getWindow().getScaledWidth();
 		int height = MinecraftClient.getInstance().getWindow().getScaledHeight();
+		// the main panel to hold the entire GUI
 		WPlainPanel panel = new WPlainPanel();
 		panel.setSize(width, height);
+		setRootPanel(panel);
+		// panel to hold the console
 		WPlainPanel consolePanel = new WPlainPanel();
 		consolePanel.setSize(width - 15, width - 15);
+		// the console text area
 		WText debugConsole = new WText(Text.literal(String.join("\n", ClientizenDebugScreen.debug)));
 		consolePanel.add(debugConsole, 0, 0, width - 15, height - 15);
+		// scroll panel for the console
 		WScrollPanel scrollPanel = new WScrollPanel(consolePanel);
 		scrollPanel.setScrollingHorizontally(TriState.FALSE);
 		scrollPanel.setScrollingVertically(TriState.TRUE);
 		panel.add(scrollPanel, 5, 5, width - 10, height - 10);
-		setRootPanel(panel);
+
 		panel.validate(this);
 	}
 
