055bf154aa65504b224236aee3c4af7e791c46fd
Aya <31237389+tal5@users.noreply.github.com>
2023-01-09 04:05:23 +0000

    Attempted lighting improvment

--- src/main/java/com/denizenscript/clientizen/render/ClientizenAttachedEntityFeatureRenderer.java
+++ src/main/java/com/denizenscript/clientizen/render/ClientizenAttachedEntityFeatureRenderer.java
@@ -3,12 +3,16 @@ package com.denizenscript.clientizen.render;
 import com.denizenscript.clientizen.objects.EntityTag;
 import com.denizenscript.clientizen.scripts.commands.AttachCommand;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightType;
 
 import java.util.List;
 
@@ -27,6 +31,9 @@ public class ClientizenAttachedEntityFeatureRenderer<T extends Entity, M extends
 		for (EntityTag attached : attachedEntities) {
 			matrices.push();
 			matrices.scale(1, -1, 1);
+			EntityRenderer<? super Entity> renderer = MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(attached.getEntity());
+			BlockPos blockPos = entity.getBlockPos().add(0, 1, 0);
+			light = LightmapTextureManager.pack(renderer.getBlockLight(attached.getEntity(), blockPos), entity.world.getLightLevel(LightType.SKY, blockPos));
 			MinecraftClient.getInstance().getEntityRenderDispatcher().render(attached.getEntity(), 0, 0, 0, attached.getEntity().getYaw(tickDelta), tickDelta, matrices, vertexConsumers, light);
 			matrices.pop();
 		}
