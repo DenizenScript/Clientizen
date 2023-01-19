4eaac4da6ae31a796edba5972e4d596b2430c6d1
Aya <31237389+tal5@users.noreply.github.com>
2023-01-19 14:10:01 +0000

    Support grouping multiple properties

--- src/main/java/com/denizenscript/clientizen/objects/properties/PropertyRegistry.java
+++ src/main/java/com/denizenscript/clientizen/objects/properties/PropertyRegistry.java
@@ -4,41 +4,30 @@ import com.denizenscript.clientizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import net.minecraft.state.property.*;
+import net.minecraft.util.StringIdentifiable;
 
 public class PropertyRegistry {
 
     public static void register() {
-        registerBooleanProperty(Properties.EYE);
-        registerBooleanProperty(Properties.WATERLOGGED);
-        registerBooleanProperty(Properties.HANGING);
-        registerEnumProperty(Properties.BED_PART);
-        registerEnumProperty(Properties.INSTRUMENT);
-        registerIntProperty(Properties.CANDLES);
-        registerIntProperty(Properties.BITES);
+        registerBooleanProperty("switched", Properties.EYE, Properties.POWERED, Properties.ENABLED);
+        registerBooleanProperty("waterlogged", Properties.WATERLOGGED);
+        registerBooleanProperty("hanging", Properties.HANGING);
+        registerEnumProperty("bed_part", Properties.BED_PART);
+        registerEnumProperty("instrument", Properties.INSTRUMENT);
+        registerIntProperty("level", Properties.CANDLES, Properties.BITES, Properties.LEVEL_3, Properties.LEVEL_8, Properties.LEVEL_1_8, Properties.LEVEL_15);
     }
 
-    public static void registerBooleanProperty(BooleanProperty property) {
-        registerBooleanProperty(property, property.getName());
+    public static void registerBooleanProperty(String name, BooleanProperty... properties) {
+        registerPropertyGetter(new MinecraftMaterialPropertyGetter<>(name, BooleanMaterialProperty::new, properties), BooleanMaterialProperty.class);
     }
 
-    public static void registerEnumProperty(EnumProperty<?> property) {
-        registerEnumProperty(property, property.getName());
+    @SafeVarargs
+    public static <T extends Enum<T> & StringIdentifiable> void registerEnumProperty(String name, EnumProperty<T>... properties) {
+        registerPropertyGetter(new MinecraftMaterialPropertyGetter<>(name, EnumMaterialProperty::new, properties), EnumMaterialProperty.class);
     }
 
-    public static void registerIntProperty(IntProperty property) {
-        registerIntProperty(property, property.getName());
-    }
-
-    public static void registerBooleanProperty(BooleanProperty property, String name) {
-        registerPropertyGetter(new MinecraftMaterialPropertyGetter<>(name, property, BooleanMaterialProperty::new), BooleanMaterialProperty.class);
-    }
-
-    public static void registerEnumProperty(EnumProperty<?> property, String name) {
-        registerPropertyGetter(new MinecraftMaterialPropertyGetter<>(name, property, EnumMaterialProperty::new), EnumMaterialProperty.class);
-    }
-
-    public static void registerIntProperty(IntProperty property, String name) {
-        registerPropertyGetter(new MinecraftMaterialPropertyGetter<>(name, property, IntMaterialProperty::new), IntMaterialProperty.class);
+    public static void registerIntProperty(String name, IntProperty... properties) {
+        registerPropertyGetter(new MinecraftMaterialPropertyGetter<>(name, IntMaterialProperty::new, properties), IntMaterialProperty.class);
     }
 
     public static void registerPropertyGetter(MinecraftMaterialPropertyGetter<?, ?> getter, Class<? extends com.denizenscript.denizencore.objects.properties.Property> propertyClass) {
@@ -47,13 +36,19 @@ public class PropertyRegistry {
         MinecraftMaterialProperty.currentlyRegistering = null;
     }
 
+    @SuppressWarnings("unchecked")
     public record MinecraftMaterialPropertyGetter<T extends Property<V>, V extends Comparable<V>>
-            (String name, T internalProperty, MinecraftMaterialPropertySupplier<T> supplier) implements PropertyParser.PropertyGetter {
+            (String name, MinecraftMaterialPropertySupplier<T> supplier, T... internalProperties) implements PropertyParser.PropertyGetter {
 
         @Override
         public com.denizenscript.denizencore.objects.properties.Property get(ObjectTag object) {
-            if (object instanceof MaterialTag material && material.state != null && material.state.contains(internalProperty)) {
-                return supplier.create(name, material, internalProperty);
+            if (!(object instanceof MaterialTag material) || material.state == null) {
+                return null;
+            }
+            for (T internalProperty : internalProperties) {
+                if (material.state.contains(internalProperty)) {
+                    return supplier.create(name, material, internalProperty);
+                }
             }
             return null;
         }
