7c44e1aae02d31f526a0343322ec0da464342233
Aya <31237389+tal5@users.noreply.github.com>
2023-06-07 20:28:33 +0100

    Removed missed line from last commit

--- src/main/java/com/denizenscript/clientizen/objects/properties/PropertyRegistry.java
+++ src/main/java/com/denizenscript/clientizen/objects/properties/PropertyRegistry.java
@@ -15,7 +15,6 @@ public class PropertyRegistry {
     public static void register() {
         registerProperty(MaterialSwitched.class, MaterialSwitched.handledProperties);
         registerProperty(MaterialWaterlogged.class, MaterialWaterlogged.handledProperties);
-        registerProperty(MaterialHanging.class, MaterialHanging.handledProperties);
         registerProperty(MaterialHalf.class, MaterialHalf.handledProperties);
         registerProperty(MaterialInstrument.class, MaterialInstrument.handledProperties);
         registerProperty(MaterialLevel.class, MaterialLevel.handledProperties);
