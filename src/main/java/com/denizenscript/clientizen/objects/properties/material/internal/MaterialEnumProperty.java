c2b4207097bd8ca4470dd81dc6ac51cbf5d56371
Aya <31237389+tal5@users.noreply.github.com>
2023-06-14 09:48:57 +0100

    Minor naming improvement, cleanups

--- src/main/java/com/denizenscript/clientizen/objects/properties/material/internal/MaterialEnumProperty.java
+++ src/main/java/com/denizenscript/clientizen/objects/properties/material/internal/MaterialEnumProperty.java
@@ -14,14 +14,14 @@ public abstract class MaterialEnumProperty extends MaterialMinecraftProperty {
 
     public static final Map<EnumProperty<?>, Conversion<?, ?>> conversions = new HashMap<>();
 
-    record Conversion<IT extends Enum<IT> & StringIdentifiable, ET extends Enum<ET> & StringIdentifiable>(Class<ET> externalTypeClass, ET[] externalTypeConstants, IT[] internalTypeConstants,
+    record Conversion<IT extends Enum<IT> & StringIdentifiable, ET extends Enum<ET> & StringIdentifiable>(Class<ET> externalType, ET[] externalTypeConstants, IT[] internalTypeConstants,
                                                                                                           IT toRemove) {}
 
     public static <IT extends Enum<IT> & StringIdentifiable, ET extends Enum<ET> & StringIdentifiable> void convertEnum(EnumProperty<IT> internalProperty, Class<ET> externalType) {
         conversions.put(internalProperty, new Conversion<>(externalType, externalType.getEnumConstants(), internalProperty.getType().getEnumConstants(), null));
     }
 
-    public static <IT extends Enum<IT> & StringIdentifiable, ET extends Enum<ET> & StringIdentifiable> void removeSingleInstance(EnumProperty<IT> internalProperty, IT toRemove) {
+    public static <IT extends Enum<IT> & StringIdentifiable> void removeSingleInstance(EnumProperty<IT> internalProperty, IT toRemove) {
         conversions.put(internalProperty, new Conversion<>(null, null, null, toRemove));
     }
 
@@ -31,7 +31,7 @@ public abstract class MaterialEnumProperty extends MaterialMinecraftProperty {
         if (conversion == null) {
             return value;
         }
-        if (conversion.externalTypeClass != null) {
+        if (conversion.externalType != null) {
             return conversion.externalTypeConstants[((Enum<?>) value).ordinal()];
         }
         return value == conversion.toRemove ? null : value;
@@ -43,8 +43,8 @@ public abstract class MaterialEnumProperty extends MaterialMinecraftProperty {
         if (conversion == null) {
             return super.parsePropertyValue(input);
         }
-        if (conversion.externalTypeClass != null) {
-            Enum<?> external = input.asEnum(conversion.externalTypeClass);
+        if (conversion.externalType != null) {
+            Enum<?> external = input.asEnum(conversion.externalType);
             return external == null ? null : conversion.internalTypeConstants[external.ordinal()];
         }
         return input.asLowerString().equals(conversion.toRemove.asString()) ? null : super.parsePropertyValue(input);
@@ -55,7 +55,6 @@ public abstract class MaterialEnumProperty extends MaterialMinecraftProperty {
 
         @Override
         default String asString() {
-//            return CoreUtilities.toLowerCase(name());
             return name();
         }
 
