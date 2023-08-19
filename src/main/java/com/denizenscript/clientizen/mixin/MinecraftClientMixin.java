a40b5cd1ca2ddb26b05839443b4aa0249de79208
Aya <31237389+tal5@users.noreply.github.com>
2023-08-19 19:02:20 +0100

    Shadow `currentScreen`

--- src/main/java/com/denizenscript/clientizen/mixin/MinecraftClientMixin.java
+++ src/main/java/com/denizenscript/clientizen/mixin/MinecraftClientMixin.java
@@ -3,8 +3,10 @@ package com.denizenscript.clientizen.mixin;
 import com.denizenscript.clientizen.events.ScreenOpenCloseEvent;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,10 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
 
+    @Shadow
+    @Nullable
+    public Screen currentScreen;
+
     @Inject(method = "setScreen", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;", opcode = Opcodes.PUTFIELD))
     private void clientizen$onScreenOpened(Screen screen, CallbackInfo ci){
         if (screen != null) {
-            ScreenOpenCloseEvent.instance.handleScreenChange(screen, ((MinecraftClient) (Object) this).currentScreen, true);
+            ScreenOpenCloseEvent.instance.handleScreenChange(screen, currentScreen, true);
         }
     }
 }
