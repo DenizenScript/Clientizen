b87a139d0fc55f500e67561540db13b949b28200
Aya <31237389+tal5@users.noreply.github.com>
2023-12-14 20:14:12 +0000

    Plain panel elements meta

--- src/main/java/com/denizenscript/clientizen/scripts/containers/gui/elements/PlainPanelElement.java
+++ src/main/java/com/denizenscript/clientizen/scripts/containers/gui/elements/PlainPanelElement.java
@@ -12,6 +12,24 @@ import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptConta
 
 public class PlainPanelElement implements GuiScriptContainer.GuiElementParser {
 
+    // <--[language]
+    // @name Plain Panel GUI Element
+    // @group GUI System
+    // @description
+    // Plain panels are the most basic type of panels, allowing their contained elements full control; they have a UI type of "plain_panel".
+    // Unlike other panel types, plain panels do not organize their contained elements in any way,
+    // giving them complete freedom to directly set their position/location.
+    //
+    // <code>
+    // ui_type: plain_panel
+    // # The plain panel's insets, optional.
+    // insets: <GUI Insets>
+    // # The plain panel's contained elements, optional.
+    // content:
+    //     <key>: <GUI Element>
+    // </code>
+    // -->
+
     @Override
     public WWidget parse(GuiScriptContainer container, YamlConfiguration config, String pathToElement, TagContext context) {
         WPlainPanel plainPanel = new WPlainPanel();
