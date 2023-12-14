91ad4046fd00d9e9e2a2ba1ddadf9394b6e47663
Aya <31237389+tal5@users.noreply.github.com>
2023-12-14 17:14:07 +0000

    Button meta, minor cleanups, remove event for now

--- src/main/java/com/denizenscript/clientizen/scripts/containers/gui/elements/ButtonElement.java
+++ src/main/java/com/denizenscript/clientizen/scripts/containers/gui/elements/ButtonElement.java
@@ -1,6 +1,5 @@
 package com.denizenscript.clientizen.scripts.containers.gui.elements;
 
-import com.denizenscript.clientizen.events.ClientizenGuiButtonPressedScriptEvent;
 import com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer;
 import com.denizenscript.clientizen.util.impl.ClientizenScriptEntryData;
 import com.denizenscript.denizencore.scripts.ScriptEntry;
@@ -17,29 +16,56 @@ import java.util.List;
 
 import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer.getSubPath;
 import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer.getTaggedEnum;
+import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer.getTaggedInt;
 import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer.getTaggedString;
 import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer.getWidgetId;
 import static com.denizenscript.clientizen.scripts.containers.gui.GuiScriptContainer.parseIcon;
 
 public class ButtonElement implements GuiScriptContainer.GuiElementParser {
 
+    // <--[language]
+    // @name Button GUI Element
+    // @group GUI System
+    // @description
+    // Buttons are clickable GUI elements that can run code when clicked, with the UI type "button".
+    //
+    // <code>
+    // ui_type: button
+    // # A label for the button, optional.
+    // label: <text>
+    // # An icon for the button, optional.
+    // icon: <GUI Icon>
+    // # The icon's size (width and height), optional.
+    // icon_size: <number>
+    // # Horizontal text alignment for the button's label, optional.
+    // horizontal_text_alignment: LEFT/CENTER/RIGHT
+    // # Script to run when the button is pressed, optional.
+    // on_click:
+    // - <script>
+    // </code>
+    // -->
+
     @Override
     public WWidget parse(GuiScriptContainer container, YamlConfiguration config, String pathToElement, TagContext context) {
-        String label = getTaggedString(config, "label", context);
         Icon icon = parseIcon(config, "icon", context);
-        WButton button = new WButton(icon, label != null ? Text.literal(label) : null);
+        WButton button = new WButton(icon);
+        String label = getTaggedString(config, "label", context);
+        if (label != null) {
+            button.setLabel(Text.literal(label));
+        }
+        Integer iconSize = getTaggedInt(config, "icon_size", context);
+        if (iconSize != null) {
+            button.setIconSize(iconSize);
+        }
         HorizontalAlignment horizontalTextAlignment = getTaggedEnum(HorizontalAlignment.class, config, "horizontal_text_alignment", context);
         if (horizontalTextAlignment != null) {
             button.setAlignment(horizontalTextAlignment);
         }
         final List<ScriptEntry> onClick = container.getEntries(new ClientizenScriptEntryData(), getSubPath(pathToElement, "on_click"));
-        final String queueName = onClick != null ? getWidgetId(pathToElement) + "_pressed" : null;
-        button.setOnClick(() -> {
-            if (onClick != null) {
-                ScriptUtilities.createAndStartQueueArbitrary(queueName, onClick, null, null, null);
-            }
-            ClientizenGuiButtonPressedScriptEvent.instance.handleButtonPress(pathToElement);
-        });
+        if (onClick != null) {
+            final String queueName = getWidgetId(pathToElement) + "_pressed";
+            button.setOnClick(() -> ScriptUtilities.createAndStartQueueArbitrary(queueName, onClick, null, null, null));
+        }
         return button;
     }
 }
