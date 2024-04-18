cf9f0a0f184ca042843b65f475dfa76730beb228
Aya <31237389+tal5@users.noreply.github.com>
2024-04-18 16:38:53 +0100

    Particle constructor meta (and friends)

--- src/main/java/com/denizenscript/clientizen/objects/ClientizenObjectRegistry.java
+++ src/main/java/com/denizenscript/clientizen/objects/ClientizenObjectRegistry.java
@@ -13,11 +13,59 @@ public class ClientizenObjectRegistry {
     public static ObjectType<ParticleTag> TYPE_PARTICLE;
 
     public static void registerObjects() {
+
+        // <--[tag]
+        // @attribute <entity[<entity>]>
+        // @returns EntityTag
+        // @description
+        // Returns an entity object constructed from the input value.
+        // Refer to <@link ObjectType EntityTag>.
+        // -->
         TYPE_ENTITY = ObjectFetcher.registerWithObjectFetcher(EntityTag.class, EntityTag.tagProcessor).setAsNOtherCode().generateBaseTag();
+
+        // <--[tag]
+        // @attribute <location[<location>]>
+        // @returns LocationTag
+        // @description
+        // Returns a location object constructed from the input value.
+        // Refer to <@link ObjectType LocationTag>.
+        // -->
         TYPE_LOCATION = ObjectFetcher.registerWithObjectFetcher(LocationTag.class, LocationTag.tagProcessor).setAsNOtherCode().setCanConvertStatic().generateBaseTag();
+
+        // <--[tag]
+        // @attribute <material[<material>]>
+        // @returns MaterialTag
+        // @description
+        // Returns a material object constructed from the input value.
+        // Refer to <@link ObjectType MaterialTag>.
+        // -->
         TYPE_MATERIAL = ObjectFetcher.registerWithObjectFetcher(MaterialTag.class, MaterialTag.tagProcessor).generateBaseTag();
+
+        // <--[tag]
+        // @attribute <item[<item>]>
+        // @returns ItemTag
+        // @description
+        // Returns an item object constructed from the input value.
+        // Refer to <@link ObjectType ItemTag>.
+        // -->
         TYPE_ITEM = ObjectFetcher.registerWithObjectFetcher(ItemTag.class, ItemTag.tagProcessor).setAsNOtherCode().generateBaseTag();
+
+        // <--[tag]
+        // @attribute <mod[<mod>]>
+        // @returns ModTag
+        // @description
+        // Returns a mod object constructed from the input value.
+        // Refer to <@link ObjectType ModTag>.
+        // -->
         TYPE_MOD = ObjectFetcher.registerWithObjectFetcher(ModTag.class, ModTag.tagProcessor).setAsNOtherCode().setCanConvertStatic().generateBaseTag();
+
+        // <--[tag]
+        // @attribute <particle[<particle>]>
+        // @returns ParticleTag
+        // @description
+        // Returns a particle object constructed from the input value.
+        // Refer to <@link ObjectType ParticleTag>.
+        // -->
         TYPE_PARTICLE = ObjectFetcher.registerWithObjectFetcher(ParticleTag.class, ParticleTag.tagProcessor).setAsNOtherCode().generateBaseTag();
     }
 }
